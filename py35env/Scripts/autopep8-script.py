c1d7054f9095430e6bf77f929d832d5c7bef05f2
Nobuho <novufo13@gmail.com>
2019-11-13 23:10:46 +0900

    make env

---
+++ py35env/Scripts/autopep8-script.py
@@ -0,0 +1,10 @@
+#!c:\_python\191111_ifcandneo4j\py35env\scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'autopep8==1.4.4','console_scripts','autopep8'
+__requires__ = 'autopep8==1.4.4'
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.exit(
+        load_entry_point('autopep8==1.4.4', 'console_scripts', 'autopep8')()
+    )
