efa97211ce5ecbaa03e37d6e8c16657f1fd03f87
Nobuho <novufo13@gmail.com>
2019-11-14 00:00:54 +0900

    edit

---
+++ ifc_perse_test.py
@@ -0,0 +1,22 @@
+import ifcopenshell
+
+print("のぶほだよ")
+
+# ifc_file = ifcopenshell.open('tebuilding_original.ifc')
+
+# products = ifc_file.by_type('IfcProduct')
+
+# for product in products:
+#     print(product)
+#     print(product.is_a())
+
+# wall = ifc_file.by_type('IfcWall')[0]
+# print(wall.GlobalId)
+# print(wall.Name)
+
+# wall = ifc_file.by_type('IfcWall')[0]
+# for definition in wall.IsDefinedBy:
+#     property_set = definition.RelatingPropertyDefinition
+#     print(property_set.Name)  # Might return Pset_WallCommon
+
+#     http://terminal integrated shellargs windows
